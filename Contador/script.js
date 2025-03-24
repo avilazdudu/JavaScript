8a716bb30d18de761d900b10041ec28c9169489a
Eduardo Ávila <ea618369@gmail.com>
2025-03-24 17:57:14 -0300

    Add files via upload

---
+++ Contador/script.js
@@ -0,0 +1,11 @@
+let btnSoma = document.getElementById('soma')
+btnSoma.addEventListener('click', () => {
+    let number = document.getElementById('number')
+    let n = 0;
+    let soma = n + 1 
+    number.innerHTML = soma;
+});
+let btnSub = document.getElementById('sub')
+btnSub.addEventListener('click', () => {
+    let number = document.getElementById('number')
+})
