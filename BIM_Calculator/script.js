c30f927c5050dce667a086c719b46f0b6c05a539
Eduardo Ávila <ea618369@gmail.com>
2025-03-26 12:23:28 -0300

    final

--- BIM_Calculator/script.js
+++ BIM_Calculator/script.js
@@ -2,9 +2,23 @@ const btnCalculate = document.getElementById('buttonCalculate');
 const height = document.getElementById('height');
 const weight = document.getElementById('weight');
 const box = document.getElementById('box');
-var result = document.createElement('div');
+const result = document.getElementById('result');
+const type = document.getElementById('type');
 
 btnCalculate.addEventListener('click', () => {
   const bmi = weight.value / Math.pow(height.value, 2);
-    document.body.insertBefore(result, btnCalculate);
+  result.innerHTML = `Your BMI is ${bmi.toFixed(2)}`;
+  if(bmi < 18.5){
+    type.innerHTML = 'You are Underweight';
+  }else if(bmi >= 18.5 && bmi <= 24.9){
+    type.innerHTML = 'You are Normal weight';
+  }else if(bmi >= 25 && bmi <= 29.9){
+    type.innerHTML = 'You are Overweight';
+  }else if(bmi >= 30 && bmi <= 34.9){
+    type.innerHTML = 'You are Obesity I';
+  }else if(bmi >= 35 && bmi <= 39.9){
+    type.innerHTML = 'You are Obesity II';
+  }else if(bmi >= 40){
+    type.innerHTML = 'You are Obesity III';
+  }
 })
